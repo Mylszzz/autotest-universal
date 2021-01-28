e410e002bfc8525a02b50c6e1314ecae73093a9e
Mylszzz <4evermyls@gmail.com>
2021-01-28 18:29:52 +0800

    Revert "Revert "update:validateOrderInfo.ts""
    This reverts commit 311d26bf4dec0684207ed55c0ad9b3cd2205b04d.

---
+++ out/testactions/validateOrderInfo.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ValidateOrderInfo = void 0;
+const deviceName_1 = require("../static/deviceName");
+class ValidateOrderInfo {
+    constructor() {
+        this.deviceName = deviceName_1.DeviceName.getDeviceName();
+    }
+}
+exports.ValidateOrderInfo = ValidateOrderInfo;
---
+++ src/testactions/validateOrderInfo.ts
@@ -0,0 +1,7 @@
+import {DeviceName} from "../static/deviceName";
+
+export class ValidateOrderInfo {
+    deviceName:string = DeviceName.getDeviceName();
+
+
+}
