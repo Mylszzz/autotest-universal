7cac7b04e4ba1e0104ef469c03277cc1f3ff1165
Daniel-LIZifan <2387599520@qq.com>
2021-01-28 16:41:16 +0800

    update screen

--- src/testactions/Screen.ts
+++ src/testactions/Screen.ts
@@ -2,6 +2,7 @@ import {Search} from "./Search";
 import {TouchAction} from "./TouchAction";
 
 export class Screen {
+    //筛选条件
     public static async screenNo(client: any,date:string,) {
         await Search.search(client);
         //点击筛选
@@ -17,11 +18,21 @@ export class Screen {
         let okView = await client.$('//android.view.View[@content-desc="确定"]');
         await okView.click();
         //选择条件
-        let typeView = await client.$('//android.view.View[@content-desc="一般销售单"]');
-        await typeView.click();
+        let typeView1 = await client.$('//android.view.View[@content-desc="一般销售单"]');
+        await typeView1.click();
+        let typeView2 = await client.$('//android.view.View[@content-desc="已完成"]');
+        await typeView2.click();
+    }
 
+    public static async okScreen(client:any){
         //完成
         let ok = await client.$('////android.widget.Button[@content-desc="完成"]');
+        await ok.click();
+    }
+
+    public static async refScreen(client:any){
+        //重置
+        let ok = await client.$('////android.widget.Button[@content-desc="重置"]');
         await ok.click();
     }
 }
